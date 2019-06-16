7f4ab3532e2582370e0d74f2fa5b6953694f8cbf
Wallace Sidhrée <i@dreamyguy.com>
2019-06-16 18:29:35 +0200

    Updates

---
+++ precache-manifest.51ac90b44b75b8b686d9c754890987a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95987c7813d4b9b0cda5b4129b1eb37",
+    "url": "/fork-me-on-github/index.html"
+  },
+  {
+    "revision": "e66472a4fcab8c66582f",
+    "url": "/fork-me-on-github/static/css/main.ce35412f.chunk.css"
+  },
+  {
+    "revision": "24d8d38260c9d300aff3",
+    "url": "/fork-me-on-github/static/js/2.c947bdef.chunk.js"
+  },
+  {
+    "revision": "e66472a4fcab8c66582f",
+    "url": "/fork-me-on-github/static/js/main.ecd6d092.chunk.js"
+  },
+  {
+    "revision": "83cb043a0dc16e2f61e6",
+    "url": "/fork-me-on-github/static/js/runtime~main.61583c1b.js"
+  }
+]);
